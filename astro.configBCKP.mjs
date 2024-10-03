123ce2ab83cc9167f769773a5a80b90b58a1f5b2
Franklin Padrón <franklinpadron@msn.com>
2024-10-03 11:47:24 -0400

    Add Cloudflare

--- astro.configBCKP.mjs
+++ astro.configBCKP.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'hybrid',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
