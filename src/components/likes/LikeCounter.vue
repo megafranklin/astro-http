4b5afc17f782d59cb6b68aa02e6cd3394d2265df
Franklin Padrón <franklinpadron@msn.com>
2024-10-20 08:25:35 -0400

    Third Commit

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -20,7 +20,7 @@
 
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import confetti from 'canvas-confetti';
 
 
@@ -35,6 +35,20 @@ const props = defineProps<Props>();
 const likeCount = ref(0);
 const likeClicks = ref(0);
 const isLoading = ref(true);
+
+watch( likeCount, () => {
+    // console.log('New likes', likeCount.value );
+    fetch(`/api/posts/likes/${ props.postId }`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ likes: likeClicks.value })
+    });
+
+    likeClicks.value = 0;
+
+});
 
 const likePost = () => {
     // console.log('+1 like');
