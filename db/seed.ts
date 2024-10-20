c7a0c69d0184e340c145fcf32d97306d25235c4f
Franklin Padrón <franklinpadron@msn.com>
2024-10-20 07:46:59 -0400

    Second Commit

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,26 @@
-import { Clients, db } from 'astro:db';
+import { getCollection } from 'astro:content';
+import { Clients, db, Posts } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
   await db.insert(Clients).values([
-    { name: 'Kasim', age: 35, isActive: true },
-    { name: 'Fernando', age: 25, isActive: true },
-    { name: 'Carlos', age: 37, isActive: true },
-    { name: 'Melissa', age: 25, isActive: false },
-    { name: 'Emin', age: 15, isActive: true },
+    { name: 'Karenis', age: 50, isActive: true },
+    { name: 'Esther', age: 50, isActive: false },
+    { name: 'Franny', age: 31, isActive: true },
+    { name: 'Daniela', age: 31, isActive: false },
+    { name: 'Franklin', age: 60, isActive: true },
+    { name: 'José', age: 60, isActive: false },
   ]);
+
+  const posts = await getCollection('blog');
+
+  await db.insert(Posts).values(
+    posts.map(p => ({
+      id: p.id,
+      title: p.data.title,
+      likes: Math.round( Math.random() * 100 ),
+    }))
+  );
 
   console.log('Seed executed');
 }
---
+++ src/components/likes/LikeCounter.vue
@@ -0,0 +1,87 @@
+<template>
+
+    <div v-if="isLoading">
+        Loading...
+    </div>
+
+    <button v-else-if="likeCount === 0" @click="likePost">
+        Like this post
+    </button>
+
+    <button v-else @click="likePost">
+        Likes
+        <span>{{ likeCount }}</span>
+    </button>
+
+    {{ likeClicks }}
+
+</template>
+
+
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import confetti from 'canvas-confetti';
+
+
+interface Props {
+    postId: string;
+}
+
+const props = defineProps<Props>();
+
+// console.log(props.postId);
+
+const likeCount = ref(0);
+const likeClicks = ref(0);
+const isLoading = ref(true);
+
+const likePost = () => {
+    // console.log('+1 like');
+    // likeCount.value = likeCount.value + 1;
+    likeCount.value++;
+    likeClicks.value++;
+
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin:{
+            x: Math.random(),
+            y: Math.random() - 0.2,
+        }
+    });
+};
+
+const getCurrentLikes = async() => {
+
+    const resp = await fetch(`/api/posts/likes/${ props.postId }`);
+    if (!resp.ok) return;
+
+    const data = await resp.json();
+    // console.log( data );
+
+    likeCount.value = data.likes;
+    isLoading.value = false;
+
+};
+
+getCurrentLikes();
+
+</script>
+
+
+<style scoped>
+button {
+    background-color: #5e51bc;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.3s;
+}
+
+button:hover {
+    background-color: #4a3f9a;
+}
+</style>
