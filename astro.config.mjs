88e1764a6e5d2974a35b309c79034b1a409191a7
Franklin Padrón <franklinpadron@msn.com>
2024-11-05 03:48:51 -0400

    Add Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,21 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+
+import cloudflare from '@astrojs/cloudflare';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+
+  // output: 'hybrid',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+
+  output: 'server',
+  adapter: cloudflare(),
 });
